fb344ad0495074016a49e5a47a750fbe40461f2f
Iuri Rezende Souza <riuri@rocketmail.com>
2014-10-08 10:22:30 -0300

    xgettext: created a bash script for parsing completions

--- fish_xgettext.sh
+++ fish_xgettext.sh
@@ -34,4 +34,29 @@ msgstr ""\
 
 }
 
-pot_output $*
+compatibility() {
+	sed 's/.*\(--description.*\)/\1/'
+}
+
+transform_dir() {
+	cd $1
+	mkdir transform original
+	for i in *.fish
+	do
+		compatibility <"$i" >transform/"$i"
+		mv "$i" original/
+	done
+	mv transform/*.fish ./
+	cd -
+	xgettext -j -k_ -kN_ -k--description -LShell --from-code=UTF-8 $1/*.fish -o messages.pot
+	xgettext_status=$?
+	cd $1
+	rm *.fish
+	mv original/*.fish ./
+	rmdir transform original
+	cd -
+	return $xgettext_status
+}
+
+#pot_output $*
+transform_dir share/completions && transform_dir share/functions
